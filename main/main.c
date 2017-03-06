e1a11d6d0712dbe0a5f6f2ee9b6756d1e3d3c71f
ChristianMoe <ic14b027@technikum-wien.at>
2017-03-06 20:35:24 +0100

    Lichtschranke

--- main/main.c
+++ main/main.c
@@ -68,6 +68,13 @@
 #include "vscp_class.h"
 #include "vscp_type.h"
 #include "vscp_firmware.h"
+
+//******************************************************************
+// SPECIAL INCLUDES - ESP32 - HW Lichtschranke
+//******************************************************************
+#include "lichtschranke.h"
+
+
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
@@ -114,7 +121,8 @@ esp_err_t event_handler(void *ctx, system_event_t *event)
  */
 void app_main(void)
 {
-    nvs_flash_init();
+    app_lichtschranke();
+	nvs_flash_init();
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
