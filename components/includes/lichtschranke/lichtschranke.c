e543ac884c5d5dec643c2ac79397803268a65620
ChristianMoe <ic14b027@technikum-wien.at>
2017-03-07 16:34:29 +0100

    Lichtschranke entprelld --- alles kompiliert nach wie vor ;-)

--- components/includes/lichtschranke/lichtschranke.c
+++ components/includes/lichtschranke/lichtschranke.c
@@ -1,11 +1,23 @@
-/* GPIO Example
+/**
+ * @file 		lichtschranke.c
+ *
+ * @brief 		GPIO Interrupt for light barrier.
+ *
+ * @details 	This Code implements the logic for the light barrier.
+ * @details		There are two light barriers in serial. One of them is attached to GPIO 2,
+ * @details		the other one is attached to GPIO 4. On both GPIOs there is an interrupt
+ * @details		triggered on each edge of the signal, that means when someone interrupts the
+ * @details		light barrier and also when the light barrier is left.
+ *
+ *
+ * @author 		Christian Mödlhammer
+ *
+ * @version 	0.1
+ * @date		07.03.2017
+ *
+ */
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/* +++++++++++++++++++++++++++ general includes +++++++++++++++++++++++++++++ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,76 +26,102 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "lichtschranke.h"
+#include "time.h"
+#include <sys/time.h>
+#include <inttypes.h>
 
-/**
- * Brief:
- * This test code shows how to configure gpio and how to use gpio interrupt.
- *
- * GPIO status:
- * GPIO18: output
- * GPIO19: output
- * GPIO4:  input, pulled up, interrupt from rising edge and falling edge
- * GPIO5:  input, pulled up, interrupt from rising edge.
- *
- * Test:
- * Connect GPIO18 with GPIO4
- * Connect GPIO19 with GPIO5
- * Generate pulses on GPIO18/19, that triggers interrupt on GPIO4/5
- *
- */
 
-#define GPIO_OUTPUT_IO_0    18
-#define GPIO_OUTPUT_IO_1    19
-#define GPIO_OUTPUT_PIN_SEL  ((1<<GPIO_OUTPUT_IO_0) | (1<<GPIO_OUTPUT_IO_1))
+/* +++++++++++++++++++++++++++ defines +++++++++++++++++++++++++++++ */
+#define __STDC_FORMAT_MACROS  // define for use of uint64_t in printf
 #define GPIO_INPUT_IO_0     4
-#define GPIO_INPUT_IO_1     5
+#define GPIO_INPUT_IO_1     2
 #define GPIO_INPUT_PIN_SEL  ((1<<GPIO_INPUT_IO_0) | (1<<GPIO_INPUT_IO_1))
 #define ESP_INTR_FLAG_DEFAULT 0
 
+/* +++++++++++++++++++++++++++ global queue +++++++++++++++++++++++++++++ */
 static xQueueHandle gpio_evt_queue = NULL;
+
+/* +++++++++++++++++++++++++++ śtruct for timevalue +++++++++++++++++++++++++++++ */
+struct timeval tv = { .tv_sec = 0, .tv_usec = 0 };   /* btw settimeofday() is helpfull here too*/
+
+/**
+ * @brief 	Interrupt Service routine for GPIO 2 or 4 trigger on rising or falling edge
+ *
+ * @param	arg	 GPIO Port that triggered an interrupt
+ *
+ */
 
 void IRAM_ATTR gpio_isr_handler(void* arg)
 {
-	gpio_num_t gpio_num_ir=(gpio_num_t) arg;
-	// Interrupt kurzzeitig deaktivieren
-	gpio_isr_handler_remove(gpio_num_ir);
-	// 10ms warten
-	vTaskDelay(10 / portTICK_RATE_MS);
-	//interrupt wieder aktivieren
-	gpio_isr_handler_add(gpio_num_ir, gpio_isr_handler, (void*) gpio_num_ir);
 	uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+	uint32_t sec, us;
+	uint64_t ts,ts_init,ts_next;
+
+	gettimeofday(&tv, NULL);
+	(sec) = tv.tv_sec;
+	(us) = tv.tv_usec;
+	(ts) = (uint64_t)(sec*1000000+us);
+	ts_init=ts;
+	ts_next=ts;
+	while ((ts_next-ts_init)<20000){ // wait for 20 milliseconds
+		gettimeofday(&tv, NULL);
+		(sec) = tv.tv_sec;
+		(us) = tv.tv_usec;
+		(ts) = (uint64_t)(sec*1000000+us);
+		ts_next=ts;
+	}
+
+	xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL); // post to queue
 }
 
-void gpio_task_example(void* arg)
+
+/**
+ * @brief 	Task for calculating the light barrier logic out of the message queue
+ *
+ * @param	arg	 none, but needed according to specification
+ *
+ */
+
+void ligthbarrier_task(void* arg)
 {
     uint32_t io_num;
+
+	// START the following code is for test purposes
+    uint32_t sec, us;
+	uint64_t ts;
+	// END the following code is for test purposes
+
     for(;;) {
         if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
             printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+    		// START the following code is for test purposes
+            gettimeofday(&tv, NULL);
+    		(sec) = tv.tv_sec;
+    		(us) = tv.tv_usec;
+    		(ts) = (uint64_t)(sec*1000000+us);
+            printf("Timestamp: %" PRIu64 "\n", ts);
+    		// END the following code is for test purposes
         }
     }
 }
 
+
+
+/**
+ * @brief 	Entry point of the light barrier logic
+ *
+ * @details 	Here the GPIO Pins are initialized, bound to the interrupt and
+ * @details 	a task is created for the calculation logic.
+ *
+ */
+
 void app_lichtschranke()
 {
     gpio_config_t io_conf;
-    //disable interrupt
-    //io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-    //set as output mode
-    //io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    //io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    //io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    //io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    //gpio_config(&io_conf);
 
     //interrupt of rising edge
     io_conf.intr_type = GPIO_INTR_NEGEDGE;
-    //bit mask of the pins, use GPIO4/5 here
+    //bit mask of the pins, use GPIO2/4 here
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     //set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
@@ -93,11 +131,21 @@ void app_lichtschranke()
 
     //change gpio intrrupt type for one pin
     gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
-
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     //start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+	//    • pvTaskCode – Pointer to the task function. In C programming, we can simply
+	//    supply the name of a function or, as has been seen in some samples, the
+	//    address of the name of the function. Apparently these equate to items that are
+	//    close enough to be used interchangablly.
+	//    • pcName – Debugging name of the task.
+	//    • usStackDepth – Size of the stack for the task.
+	//    • pvParameters – Parameters for the task instance. This may be NULL.
+	//    • uxPriority – Priority of the task instance.
+	//    • xTaskHandle – Reference to the newly created task instance. This may be
+	//    passed in as NULL if no task handle is required to be returned.
+
+    xTaskCreate(ligthbarrier_task, "ligthbarrier_task", 2048, NULL, 10, NULL);
 
     //install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
@@ -108,16 +156,12 @@ void app_lichtschranke()
 
     //remove isr handler for gpio number.
     //gpio_isr_handler_remove(GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin again
-    //gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
 
-    int cnt = 0;
+    //int cnt = 0;
     while(1) {
 
-    	printf("cnt: %d\n", cnt++);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-//        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
-//        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
+    //	printf("cnt: %d\n", cnt++);
+    //    vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
 
